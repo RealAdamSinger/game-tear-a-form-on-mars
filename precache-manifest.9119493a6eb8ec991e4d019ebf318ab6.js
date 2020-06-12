4c6b89c529d7d09c4484f426865fd76bd6c97030
Adam SInger <asinger@majik.io>
2020-06-12 00:05:34 -0400

    Updates

---
+++ precache-manifest.9119493a6eb8ec991e4d019ebf318ab6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4d4bf8eb102e9153e69a",
+    "url": "/game-tear-a-form-on-mars/static/css/main.0282dce6.chunk.css"
+  },
+  {
+    "revision": "4d4bf8eb102e9153e69a",
+    "url": "/game-tear-a-form-on-mars/static/js/main.e530d560.chunk.js"
+  },
+  {
+    "revision": "26dfa4a2fdee84ec8f86",
+    "url": "/game-tear-a-form-on-mars/static/js/runtime~main.19c80354.js"
+  },
+  {
+    "revision": "07eaf3c48739f7b27e46",
+    "url": "/game-tear-a-form-on-mars/static/js/2.600506b1.chunk.js"
+  },
+  {
+    "revision": "75adeef565d11995bebac88758cc7242",
+    "url": "/game-tear-a-form-on-mars/index.html"
+  }
+];
